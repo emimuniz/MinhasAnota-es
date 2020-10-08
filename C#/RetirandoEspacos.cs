f47be7e1d1ffd7bd34dbd05851c84f58b869e53a
Emillyn Muniz <emillyn_muniz@hotmail.com>
2020-10-07 22:17:16 -0300

    Create RetirandoEspacos.cs

---
+++ C#/RetirandoEspacos.cs
@@ -0,0 +1,14 @@
+Console.WriteLine();
+//Cortar os espaços das cadeias de caracteres
+string greeting = "      Hello World!       ";
+Console.WriteLine($"[{greeting}]");
+
+string trimmedGreeting = greeting.TrimStart();
+Console.WriteLine($"[{trimmedGreeting}]");
+
+trimmedGreeting = greeting.TrimEnd();
+Console.WriteLine($"[{trimmedGreeting}]");
+
+trimmedGreeting = greeting.Trim();
+Console.WriteLine($"[{trimmedGreeting}]");
+
