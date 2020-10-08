5fa1c837888ccf6706527db774b35b8303a570b3
Emillyn Muniz <emillyn_muniz@hotmail.com>
2020-10-07 22:18:52 -0300

    Create ContemCaractere.cs

---
+++ C#/ContemCaractere.cs
@@ -0,0 +1,9 @@
+
+  Console.WriteLine();
+
+  //Verificando se contem o caractere
+  string songLyrics = "You say goodbye, and I say hello";
+  Console.WriteLine(songLyrics.Contains("goodbye"));
+  Console.WriteLine(songLyrics.Contains("greetings"));
+  Console.WriteLine(songLyrics.StartsWith("You"));
+  Console.WriteLine(songLyrics.EndsWith("hello"));
