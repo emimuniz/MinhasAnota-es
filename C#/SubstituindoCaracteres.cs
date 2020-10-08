79cef3ce4dad1c40492f5d83f2f83fc65af64465
Emillyn Muniz <emillyn_muniz@hotmail.com>
2020-10-07 22:18:11 -0300

    Create SubstituindoCaracteres.cs

---
+++ C#/SubstituindoCaracteres.cs
@@ -0,0 +1,11 @@
+
+  Console.WriteLine();
+  //Substituindo Caracteres
+  string sayHello = "Hello World!";
+  Console.WriteLine(sayHello);
+  sayHello = sayHello.Replace("Hello", "Greetings");
+  Console.WriteLine(sayHello);
+
+  //colocando em letra maiuscula ou minuscula 
+  Console.WriteLine(sayHello.ToUpper());
+  Console.WriteLine(sayHello.ToLower());
