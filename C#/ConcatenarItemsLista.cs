19c3063083c5fc32db125e094535e3930da49d80
Emillyn Muniz <emillyn_muniz@hotmail.com>
2020-10-26 17:52:42 -0300

    Update ConcatenarItemsLista.cs

--- C#/ConcatenarItemsLista.cs
+++ C#/ConcatenarItemsLista.cs
@@ -1,3 +1,7 @@
 string delimiter = ",";
 List<string> items = new List<string>() { "foo", "boo", "john", "doe" };
 Console.WriteLine(items.Aggregate((i, j) => i + delimiter + j));
+
+
+///outra forma
+items.Select(i => i.Boo).Aggregate((i, j) => i + delimiter + j)
